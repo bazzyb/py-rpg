742f72e3d359e8192fdc6a69401663c0f199a13c
Daryl Boakes <darylboakes@hotmail.co.uk>
2021-05-16 18:55:36 +0100

    Basic healing added

--- app/__init__.py
+++ app/__init__.py
@@ -1,9 +1,12 @@
 class Character:
-    def __init__(self, max_health: int = 1000, level: int = 1) -> None:
-        self.__health: int = max_health
+    def __init__(self, max_health: int = 1000, level: int = 1, starting_health: int = None) -> None:
+        self.__health: int = starting_health if starting_health else max_health
         self.__max_health: int = max_health
         self.__level: int = level
         self.__alive: bool = True
+
+    def __repr__(self):
+        return f"Health: {self.__health} | Alive: {self.__alive}"
 
     def get_level(self) -> int:
         return self.__level
@@ -14,10 +17,17 @@ class Character:
     def is_alive(self) -> bool:
         return self.__alive
 
-    def attack(self, enemy: "Character") -> None:
-        enemy.__reduce_health(100)
+    def attack(self, character: "Character") -> None:
+        character.__reduce_health(100)
+
+    def heal(self, character: "Character") -> None:
+        character.__increase_health(100)
 
     def __reduce_health(self, reduce_by: int) -> None:
         self.__health = max(self.__health - reduce_by, 0)
         if self.__health == 0:
             self.__alive = False
+
+    def __increase_health(self, increase_by: int) -> None:
+        if self.__alive:
+            self.__health = min(self.__health + increase_by, self.__max_health)
--- tests/test_py_rpg.py
+++ tests/test_py_rpg.py
@@ -14,22 +14,42 @@ class TestCharacter:
         assert player.get_level() == 5
 
 
-class TestCombat:
+class TestAttacking:
     @pytest.fixture(autouse=True)
     def __prep_characters(self):
-        self.character = Character()
-        self.experienced_character = Character(max_health=3000, level=10)
-        self.injured_character = Character(max_health=100)
+        self.player = Character()
 
         self.opponent = Character()
-        self.experienced_opponent = Character(max_health=3000, level=10)
         self.injured_opponent = Character(max_health=100)
 
     def test_opponent_should_have_900_health_after_attack(self):
-        self.character.attack(self.opponent)
+        self.player.attack(self.opponent)
         assert self.opponent.get_health() == 900
 
-    def test_opponent_should_die_if_attack_exceeds_health(self):
-        self.character.attack(self.injured_opponent)
+    def test_opponent_should_be_alive_if_attack_leaves_health_above_0(self):
+        self.player.attack(self.opponent)
+        assert self.opponent.get_health() == 900
+        assert self.opponent.is_alive() is True
+
+    def test_opponent_should_die_if_attack_leaves_health_at_0(self):
+        self.player.attack(self.injured_opponent)
         assert self.injured_opponent.get_health() == 0
         assert self.injured_opponent.is_alive() is False
+
+
+class TestHealing:
+    @pytest.fixture(autouse=True)
+    def __prep_characters(self):
+        self.player = Character()
+        self.experienced_player = Character(max_health=3000, level=10)
+        self.injured_player = Character(starting_health=100)
+
+    def test_healing_should_increase_players_health(self):
+        self.player.heal(self.injured_player)
+        assert self.injured_player.get_health() == 200
+
+    def test_healing_should_not_increase_health_if_player_is_dead(self):
+        self.player.attack(self.injured_player)
+        self.player.heal(self.injured_player)
+        assert self.injured_player.get_health() == 0
+        assert self.injured_player.is_alive() == False
